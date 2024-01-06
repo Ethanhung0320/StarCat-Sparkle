89fbe3ff83438769063b8b8449c8ae9b739227b9
Ethan <q40131125@gmail.com>
2024-01-06 23:52:30 +0800

    Initial Commit

---
+++ StarCat Sparkle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarCat Sparkle
+//
+//  Created by Ethan Hung on 2024/1/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
